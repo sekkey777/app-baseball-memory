a19d89b3019e3ac129651cbf59b26ebc448efa25
sekkey777 <seki7united@gmail.com>
2023-05-03 20:41:00 +0900

    add：マスターデータの追加
    ・baseball_teamsテーブル
    ・baseball_parkテーブル
    ・categoriesテーブル

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p '=========================== START ==========================='
+
+p '=========================== CREATE BASEBALL-TEAMS ==========================='
+
+baseball_teams = %W(
+  東京ヤクルトスワローズ
+  横浜DeNAベースターズ
+  阪神タイガース
+  読売ジャイアンツ
+  広島東洋カープ
+  中日ドラゴンズ
+  オリックス・バッファローズ
+  福岡ソフトバンクフォークス
+  埼玉西武ライオンズ
+  東北楽天ゴールデンイーグルス
+  千葉ロッテマリーンズ
+  北海道日本ハムファイターズ
+)
+
+BaseballTeam.create!(baseball_teams.map { |name| { name: name } })
+
+p '=========================== CREATE BASEBALL-PARKS ==========================='
+
+baseball_parks = %W(
+  明治神宮球場
+  横浜スタジアム
+  阪神甲子園球場
+  東京ドーム
+  MAZDA\ Zoom-Zoom\ スタジアム\ 広島
+  バンテリンドーム\ ナゴヤ
+  京セラドーム
+  福岡PayPayドーム
+  ベルーナドーム
+  楽天生命パーク宮城
+  ZOZOマリンスタジアム
+  エスコンフィールド\ HOKKAIDO
+)
+
+BaseballPark.create!(baseball_parks.map { |name| { name: name } })
+
+p '=========================== CREATE CATEGORIES ==========================='
+
+categories = %W(
+  グルメ
+  座席からの風景
+  あなたのMVP
+  その他
+)
+
+Category.create!(categories.map { |name| { name: name } })
+
+p '=========================== END ==========================='
