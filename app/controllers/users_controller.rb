b2ea41a94c9ae763443edfd6f369a052768d73c0
sekkey777 <seki7united@gmail.com>
2023-05-04 23:56:56 +0900

    feat：ユーザー登録成功時のflashメッセージを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = 'アカウントを登録しました。'
       redirect_to user_path(@user)
     else
       render 'new'
