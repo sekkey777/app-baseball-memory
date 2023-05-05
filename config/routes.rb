f898cc0924db2121e786cba7623fa494be69a998
sekkey777 <seki7united@gmail.com>
2023-05-05 20:35:22 +0900

    fix：ユーザー編集アクションが有効になっていなかったため修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:new, :create, :show, :edit]
+  resources :users
 end
