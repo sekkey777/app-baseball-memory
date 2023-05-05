5a82c08f1848348c4a9c3341b66aaf84aafe8993
sekkey777 <seki7united@gmail.com>
2023-05-06 05:04:30 +0900

    feat：思い出投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,20 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+    @categories = Category.all
+    @baseball_parks = BaseballPark.all
+    @baseball_teams = BaseballTeam.all
+  end
+
+  def create
+    @post = Post.new(post_params.merge(user_id: current_user.id))
+    if @post.save
+      flash[:success] = "「#{@post.title}」を投稿しました"
+      redirect_to posts_path
+    else
+      render new_post_path
+    end
   end
 
   def show
@@ -11,4 +25,9 @@ class PostsController < ApplicationController
 
   def edit
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content, :baseball_team_id, :baseball_park_id, :category_id)
+    end
 end
