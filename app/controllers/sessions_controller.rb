208f78ae19a62fb6078db4f13c6cb9928d9a1988
sekkey777 <seki7united@gmail.com>
2023-05-05 16:44:27 +0900

    fix：ログイン時、emailとpasswordの検証からnameとpasswordの検証に修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:name])
+    user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user_path(user)
