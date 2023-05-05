edb4299da64db695657b1fd50c9e3e9902351def
sekkey777 <seki7united@gmail.com>
2023-05-06 05:18:45 +0900

    feat：投稿詳細と投稿編集機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,9 +21,30 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
+    @categories = Category.all
+    @baseball_parks = BaseballPark.all
+    @baseball_teams = BaseballTeam.all
   end
 
   def edit
+    @post = Post.find(params[:id])
+    @categories = Category.all
+    @baseball_parks = BaseballPark.all
+    @baseball_teams = BaseballTeam.all
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @categories = Category.all
+    @baseball_parks = BaseballPark.all
+    @baseball_teams = BaseballTeam.all
+    if @post.update(post_params)
+      flash[:success] = '投稿内容を更新しました'
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
   end
 
   private
