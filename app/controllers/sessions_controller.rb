7985bc79e048c653c4dc67840cf8ef588b16f590
sekkey777 <seki7united@gmail.com>
2023-05-05 15:37:20 +0900

    feat：ログイン機能の実装
    ・ログインページの実装
    ・ログイン時にsessionにuser_idを保持

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'ログインに失敗しました。ユーザー名、またはパスワードが違います。'
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
 end
