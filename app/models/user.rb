661e602b8dded2a5e7e796ea43a996e12a335dac
sekkey777 <seki7united@gmail.com>
2023-05-11 04:12:02 +0900

    clean：rubocop-airbnbによる整形

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,11 +19,13 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   before_save { self.email = email.downcase }
-  
+
   validates :name, presence: true, length: { in: 8..20, allow_blank: true }, uniqueness: { case_sensitive: false }
-  validates :email, presence: true, length: { maximum: 255, allow_blank: true }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 8, allow_blank: true }, allow_nil: true, format: { with: VALID_PASSWORD_REGEX }
-  
+  validates :email, presence: true, length: { maximum: 255, allow_blank: true }, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8, allow_blank: true }, allow_nil: true,
+                       format: { with: VALID_PASSWORD_REGEX }
+
   has_secure_password
 
   has_many :posts
