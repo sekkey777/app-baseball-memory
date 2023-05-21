b5142268aa1a7849d2219987c400da40dbe67760
sekkey777 <seki7united@gmail.com>
2023-05-21 20:45:38 +0900

    clean：Gamesコントローラの共通コードをメソッドに切り出し

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: [:show, :edit, :update, :destroy]
   before_action :set_select_values, only: [:new, :edit]
 
   def new
@@ -20,15 +21,12 @@ class GamesController < ApplicationController
   end
 
   def show
-    @game = Game.find(params[:id])
   end
 
   def edit
-    @game = Game.find(params[:id])
   end
 
   def update
-    @game = Game.find(params[:id])
     if @game.update(game_params)
       flash[:success] = '投稿内容を更新しました'
       redirect_to games_path
@@ -38,7 +36,6 @@ class GamesController < ApplicationController
   end
 
   def destroy
-    @game = Game.find(params[:id])
     @game.destroy
     flash[:success] = "「#{@game.home_team.name}」 vs 「#{@game.away_team.name}」の観戦記録を削除しました"
     redirect_to games_path
@@ -49,4 +46,9 @@ class GamesController < ApplicationController
   def game_params
     params.require(:game).permit(:date, :memo, :home_team_id, :away_team_id, :baseball_park_id, :home_team_score, :away_team_score, :photo)
   end
+
+  def set_game
+    @game = current_user.games.find(params[:id])
+  end
+
 end
