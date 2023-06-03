34f2a4f8dd757186acdebc12ffa8d7c8da63e975
sekkey777 <seki7united@gmail.com>
2023-06-03 15:40:54 +0900

    cleam：schema情報の追記

--- spec/factories/baseball_teams.rb
+++ spec/factories/baseball_teams.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: baseball_teams
+#
+#  id         :bigint           not null, primary key
+#  name       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :baseball_team do
     name { 'baseball_team' }
