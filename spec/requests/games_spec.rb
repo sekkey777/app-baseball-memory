a79251049e6d146a2bf790a18ab3395786e5a293
sekkey777 <seki7united@gmail.com>
2023-06-09 01:21:23 +0900

    spec：観戦記録関連画面のリクエストスペックを実装

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -1,24 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe "Games", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/games/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
+RSpec.describe 'Games', type: :request do
+  let!(:game) { create(:game) }
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/games/index"
-      expect(response).to have_http_status(:success)
+  describe '観戦記録関連画面' do
+    it '観戦記録一覧画面に遷移できず、ログイン画面に遷移すること' do
+      get games_path
+      expect(response).to redirect_to(login_path)
     end
-  end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/games/show"
-      expect(response).to have_http_status(:success)
+    it '観戦記録新規登録に遷移できず、ログイン画面に遷移すること' do
+      get new_game_path
+      expect(response).to redirect_to(login_path)
+    end
+
+    it '観戦記録詳細画面に遷移できず、ログイン画面に遷移すること' do
+      get game_path(game)
+      expect(response).to redirect_to(login_path)
+    end
+
+    it '観戦記録編集画面に遷移できず、ログイン画面に遷移すること' do
+      get edit_game_path(game)
+      expect(response).to redirect_to(login_path)
     end
   end
 end
