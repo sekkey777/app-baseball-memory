fadc0cf4e0e932bc409af1dbb89bcb4e2bec6198
sekkey777 <seki7united@gmail.com>
2023-06-09 01:34:56 +0900

    update：不要なルーティングの削除

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users
+  resources :users, only: [:new, :create, :show, :edit, :update]
   resources :posts, only: [:index, :new, :create, :show] do
     resources :likes, only: [:create, :destroy]
   end
