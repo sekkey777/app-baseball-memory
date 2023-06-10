987fc010dc4c4d8d8b5ba63edfea3e161686094c
sekkey777 <seki7united@gmail.com>
2023-06-10 22:55:31 +0900

    spec：投稿時のシステムスペックを追加

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -74,4 +74,54 @@ RSpec.describe 'Posts', type: :system do
       expect(page).not_to have_content('Post2')
     end
   end
+
+  describe '投稿する' do
+    let(:user) { create(:user) }
+    let!(:baseball_team) { create(:baseball_team, name: 'Team') }
+    let!(:baseball_park) { create(:baseball_park, name: 'Park') }
+    let!(:category) { create(:category, name: 'Category') }
+    before do
+      login(user)
+      visit new_post_path
+    end
+
+    it '正常な値で投稿する' do
+      # 入力欄に入力して投稿
+      fill_in 'タイトル', with: 'post_test_title'
+      select 'Team', from: 'チーム'
+      select 'Park', from: '球場'
+      select 'Category', from: 'カテゴリー'
+      fill_in '投稿内容', with: 'post_test_content'
+      attach_file('post_photo', Rails.root.join('spec/images/test_image.jpg'))
+      click_on '投稿する'
+      # 投稿一覧に遷移
+      expect(page).to have_current_path(posts_path)
+      expect(page).to have_content('post_test_title')
+      expect(page).to have_content('Team')
+      expect(page).to have_content('Park')
+      expect(page).to have_content('Category')
+      expect(page).to have_css("img[src*='test_image.jpg']")
+    end
+
+    it '空値で投稿する' do
+      click_on '投稿する'
+      expect(page).to have_content('投稿に失敗しました。')
+      expect(page).to have_content('タイトル を入力してください')
+      expect(page).to have_content('投稿内容 を入力してください')
+      expect(page).to have_content('写真 を入力してください')
+      expect(page).to have_content('チーム は必須です')
+      expect(page).to have_content('球場 は必須です')
+      expect(page).to have_content('カテゴリー は必須です')
+    end
+
+    it '空値で投稿する' do
+      fill_in 'タイトル', with: 'x' * 81
+      fill_in '投稿内容', with: 'x' * 1001
+      click_on '投稿する'
+      expect(page).to have_content('投稿に失敗しました。')
+      expect(page).to have_content('タイトル は80文字以下で入力してください')
+      expect(page).to have_content('投稿内容 は1000文字以下で入力してください')
+    end
+
+  end
 end
