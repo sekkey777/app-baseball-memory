48d114552ddb41bf6067e352fb9e6479e9df8879
sekkey777 <seki7united@gmail.com>
2023-06-11 21:12:24 +0900

    clean：使用していない変数の削除

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    like = current_user.likes.create(post_id: @post.id)
+    current_user.likes.create(post_id: @post.id)
   end
 
   def destroy
     @post = Post.find(params[:post_id])
-    like = current_user.likes.find_by(post_id: @post.id).destroy
+    current_user.likes.find_by(post_id: @post.id).destroy
   end
 end
